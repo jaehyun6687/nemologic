46db725c1473cdc0196ddc2cae4c8920c8668e5c
jaehyun6687 <jaehyun6687@naver.com>
2020-03-05 02:11:17 +0900

    Add files via upload

---
+++ 메뉴/menu.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $("#c1").mouseenter(function () {
+        $("#c1 .button").fadeIn();
+    });
+    $("#c1").mouseleave(function () {
+        $("#c1 .button").fadeOut(100);
+    })
+    $("#c2").mouseenter(function () {
+        $("#c2 .button").fadeIn();
+    });
+    $("#c2").mouseleave(function () {
+        $("#c2 .button").fadeOut(100);
+    })
+    $("#c3").mouseenter(function () {
+        $("#c3 .button").fadeIn();
+    });
+    $("#c3").mouseleave(function () {
+        $("#c3 .button").fadeOut(100);
+    })
+    $("#c4").mouseenter(function () {
+        $("#c4 .button").fadeIn();
+    });
+    $("#c4").mouseleave(function () {
+        $("#c4 .button").fadeOut(100);
+    })
+    $("#c5").mouseenter(function () {
+        $("#c5 .button").fadeIn();
+    });
+    $("#c5").mouseleave(function () {
+        $("#c5 .button").fadeOut(100);
+    })
+    $("#c6").mouseenter(function () {
+        $("#c6 .button").fadeIn();
+    });
+    $("#c6").mouseleave(function () {
+        $("#c6 .button").fadeOut(100);
+    })
+});
